e8e1598456a31949a69f033be542bcfedd7593b7
Sourav Chakraborty <123477647+souravvvv123@users.noreply.github.com>
2023-08-04 15:33:54 +0530

    Update git-secret.py

--- git-secret.py
+++ git-secret.py
@@ -1,54 +1,130 @@
 import time
+import requests
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-scrape=input("what page would you like to scrape?")
-s = Service(ChromeDriverManager().install())
+import argparse
+import termcolor
+from termcolor import colored
+
+banner = colored('''--------------------------------------------------------------------------
+
+###############   #####  ###########        ##########  #########  #########  ########  #########  ############                
+#                   #         #             #           #          #          #      #  #               #
+#                   #         #             #           #          #          #      #  #               #
+#                   #         #  ------     #           #          #          #      #  ########        #
+#        ######     #         #             ##########  #########  #          ########  #               #
+#        #    #     #         #                      #  #          #          # #       #               #
+#        #    #     #         #                      #  #          #          #   #     #               #
+##########    #   #####       #             ##########  #########  ########## #     #   ########        #                    
+
+                                                                            AUTHOR: SOURAV CHAKRABORTY
+                                                                            VERSIONl:l 0.1        
+---------------------------------------------------------------------------------''', '''red''')
+
+print(banner)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('url', type=str, help='Enter the url to begin search')
+args = parser.parse_args()
+
+
+s = Service(ChromeDriverManager(version="114.0.5735.90").install())
 driver = webdriver.Chrome(service=s)
-driver.get(f"{scrape}")
-repo=f"{scrape}"
-res=driver.find_elements(By.CLASS_NAME,"repo")
+#url = "https://github.com/freepein"
+url=args.url
+url2 = url + "?tab=repositories"
+driver.get(url2)
+names=[]
+res = driver.find_elements(By.CLASS_NAME, "wb-break-all")
+for heading in res:
+    x=heading.text
+    y=x.replace("Public","")
+    z=names.append(y)
 
-repo_links=[]
-flink=[]
+time.sleep(1)
+my_list = []
 
-def going_for_raw(second_page):
-    driver.get(second_page)
-    raw=driver.find_element(By.CLASS_NAME,"js-permalink-replaceable-link")
-    raw.click()
-    htmll=driver.page_source
-    htmll=f"{htmll}"
-    if "password" in htmll:
-        print(f"found password:{second_page}")
-    elif "api_key" in htmll:
-        print(f"found api_key:{second_page}")
-    elif "admin" in htmll:
-        print(f"found admin in:{second_page}")
-    elif "secret_token" in htmll:
-        print(f"found secret token:{second_page}")
-    else:
-        print("couldnt find any secret")
 
-def loop(next_page):
-    global a
-    driver.get(next_page)
-    res2=driver.find_elements(By.CLASS_NAME,"js-navigation-open")
-    if "py" in a.text:
-        second_page=f"{next_page}/blob/master/{a.text}"
-        going_for_raw(second_page)
+def loop(my_list):
+    driver.get(my_list)
+    time.sleep(1)
+    driver.back()
+
 
 for i in res:
-    rep=i.text.replace("Public","").strip()
-    repo_links.append(rep)
+    i = url + "/" + i.text
+    i = i.replace("Public", "")
+    my_list.append(i)
 
-for l in repo_links:
-    next_page=f"{repo}/{l}"
-    flink.append(next_page)
-    loop(next_page)
+url_content=[]
+for j in my_list:
+    alllinks = driver.get(j)
+    time.sleep(1)
+    alllinks2 = driver.find_elements(By.CLASS_NAME, "js-navigation-open")
+    for a in alllinks2:
+        getting = a.text
+        getting = getting.replace(" ", "") # remove whitespace
+        url_content.append(getting)
+
+
+
+url_heading=[]
+whole_content=[]
+def getting_url_heading():
+    for title in names:
+        global finale
+        tit=url+"/"+title
+        finale=url_heading.append(tit)
+def parsing_url_heading():
+    for parse in url_heading:
+        for cont in url_content:
+            aa=parse+"/"+"blob/master/"+cont
+            bb=whole_content.append(aa)
+
+
+getting_url_heading()
+parsing_url_heading()
+
+listed_urls = [url.replace(" ", "") for url in whole_content] # remove whitespace
+
+working_urls=[]
+for work in listed_urls:
+    try:
+        req=requests.get(work.strip())
+        print(f"status code for {work}: {req.status_code}")
+        if req.status_code==200:
+            working_urls.append(work)
+    except requests.exceptions.RequestException as ee:
+        print(f"Error getting {work}: {ee}")
+
+print(f"working urls are: {working_urls}")
+
+def read_keywords_from_file(filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+        return [line.strip() for line in f]
+
+
+keywords_filename = 'keyword.txt'
+keywords = read_keywords_from_file(keywords_filename)
+
+
+def check_keywords_in_content(url, keywords):
+    response = requests.get(url)
+    if response.status_code == 200:
+        content = response.text
+        for keyword in keywords:
+            if keyword in content:
+                return keyword
+    return None
+
+
+for url in working_urls:
+    found_keyword = check_keywords_in_content(url, keywords)
+    if found_keyword:
+        print(f"Keyword '{found_keyword}' found in URL: {url}")
+
+
+
 driver.quit()
-
-
-
-
-
